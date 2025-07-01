bc9bd6dc0544848e9b2500624b4277088a36916c
Yashashwini11 <125336223+Yashashwini11@users.noreply.github.com>
2025-07-01 20:20:31 +0530

    Delete ANSI_SQL directory

--- ANSI_SQL/InactiveUsers.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.*
-FROM Users u
-WHERE u.user_id NOT IN (
-    SELECT user_id
-    FROM Registrations
-    WHERE registration_date >= CURDATE() - INTERVAL 90 DAY
-);
--- ANSI_SQL/UnregisteredActiveUsers.sql
+++
@@ -1,6 +0,0 @@
-SELECT u.*
-FROM Users u
-WHERE registration_date >= CURDATE() - INTERVAL 30 DAY
-AND u.user_id NOT IN (
-    SELECT DISTINCT user_id FROM Registrations
-);
